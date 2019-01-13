b63ce28fe1b4a3ddb54dfbb2e140a08f9ac99926
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-13 14:06:20 -0800

    add show implementation of SellerController

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -30,7 +30,11 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
+        $seller = Seller::has('products')->findOrFail($id);
+
+        return response()->json([
+            'data' => $seller
+        ], 200);
     }
 
 }
