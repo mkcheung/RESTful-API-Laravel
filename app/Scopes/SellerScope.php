8ba18f41e2d0d5be9f62dfc497f60a54b5c80c98
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-13 19:36:58 -0800

    add global scope to seller model for implicit model binding

--- app/Scopes/SellerScope.php
+++ app/Scopes/SellerScope.php
@@ -5,9 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
 
-class BuyerScope implements Scope
+class SellerScope implements Scope
 {
 	public function apply(Builder $builder, Model $model){
-		$builder->has('transactions');
+		$builder->has('products');
 	}
 }
