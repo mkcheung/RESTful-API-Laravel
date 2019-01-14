b60f1ce209c3f4d4a0c6c657ba9dcae32a006713
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-13 18:06:39 -0800

    alter response of validation exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,18 +2,23 @@
 
 namespace App\Exceptions;
 
+use App\Http\Controllers\ApiController;
+use App\Traits\ApiResponder;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponder;
+
     /**
      * A list of the exception types that are not reported.
      *
      * @var array
      */
     protected $dontReport = [
-        //
+        \Illuminate\Validation\ValidationException::class
     ];
 
     /**
@@ -46,6 +51,18 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceOf ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
         return parent::render($request, $exception);
+    }
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors, 422);
+
     }
 }
