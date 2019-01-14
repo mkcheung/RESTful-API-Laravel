266b1b0e49e7cfa9addaa63e3c40198f7aa4e602
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-13 18:29:26 -0800

    move authentication exception handler to child handler and refactor for json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ApiController;
 use App\Traits\ApiResponder;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
@@ -62,7 +63,23 @@ class Handler extends ExceptionHandler
             return $this->errorResponse("No {$modelName} instance exists with the specified model id.", 404);
         }
 
+        if($exception instanceOf AuthenticationException){
+            return $this->unauthenticated($request, $exception);
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated.', 401);
     }
 
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
