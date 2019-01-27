3f8cc8ee0f0ae621a826f58f0a1e5780c5480b91
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-27 15:09:13 -0800

    add seller transaction index method and route

--- app/Product.php
+++ app/Product.php
@@ -25,6 +25,9 @@ class Product extends Model
     	'image',
     	'seller_id'
     ];
+    protected $hidden = [
+        'pivot'
+    ];
 
     public function isAvailable(){
     	return $this->status == Product::AVAILABLE_PRODUCT;
