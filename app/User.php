5bb1a940501a9cd91e03b254f4b737d728be32ab
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-01-29 22:16:40 -0800

    make visible verification token

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'verification_token',
+//        'verification_token',
     ];
 
     public function setNameAttribute($name){
