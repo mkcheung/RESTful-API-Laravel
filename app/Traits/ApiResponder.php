4989d6822dae11311cb3fb537ce54d12e7a1746b
Mars Cheung <mars.kwong.cheung@gmail.com>
2019-02-03 14:30:08 -0800

    apply transformers to ApiResponse Trait

--- app/Traits/ApiResponder.php
+++ app/Traits/ApiResponder.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use function fractal;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -20,14 +21,31 @@ trait ApiResponder
 	}
 
 	protected function showAll(Collection $collection, $code = 200){
-		return $this->successResponse(['data' => $collection], $code);
+
+	    if($collection->isEmpty()){
+	        return $this->successResponse(['data' => $collection], $code);
+        }
+	    $transformer = $collection->first()->transformer;
+
+	    $collection = $this->transformData($collection, $transformer);
+
+		return $this->successResponse($collection, $code);
 	}
 
-    protected function showOne(Model $model, $code = 200){
-        return $this->successResponse(['data' => $model], $code);
+    protected function showOne(Model $instance, $code = 200){
+        $transformer = $instance->transformer;
+
+        $instance = $this->transformData($instance, $transformer);
+        return $this->successResponse($instance, $code);
     }
 
     protected function showMessage($message, $code = 200){
         return $this->successResponse(['data' => $message], $code);
+    }
+
+    protected function transformData($data, $transformer){
+        $transformation = fractal($data, new $transformer);
+
+        return $transformation->toArray();
     }
 }
